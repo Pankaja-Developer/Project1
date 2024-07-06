4ad6a068af55c49206f4298a5d1b4dec94cf8747
Pankaja-Developer <pankajasahu04@gmail.com>
2024-07-06 07:35:49 +0530

    First Project added

---
+++ Project1/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const HeroSection = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SEOES.
+          YOUR FEET DESERVE THE BEST AND WERE TO HELP YOU WITH OUR SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icons">
+            <img src="/images/amazon (1).png" alt="amazon-flipkart" />
+            <img src="/images/flipkart (2).png" alt="flipkart-logo" />
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-image">
+        <img src="/images/hero-image.png" alt="hero-image" />
+      </div>
+    </main>
+  );
+};
+
+export default HeroSection;
